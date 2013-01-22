c1b41927826a54fe86ce35fdbdeacc41a8e8b831
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-01-22 10:25:19 +0100

    add French Label

--- fileuploader.js
+++ fileuploader.js
@@ -485,8 +485,8 @@ qq.FileUploader = function(o){
         listElement: null,
                 
         template: '<div class="qq-uploader">' + 
-                '<div class="qq-upload-drop-area"><span>Drop files here to upload</span></div>' +
-                '<div class="qq-upload-button">Upload a file</div>' +
+                '<div class="qq-upload-drop-area"><span>Déposer un fichier ici</span></div>' +
+                '<div class="qq-upload-button">Déposer un fichier</div>' +
                 '<ul class="qq-upload-list"></ul>' + 
              '</div>',
 
